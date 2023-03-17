db4933637dff2adb8375fca4414a50f681cdf2c8
Chikaodili Deng <mchikky@yahoo.com>
2023-03-17 11:49:04 +0100

    Creating of weather list using extraction views using Xcode

--- Design12/ContentView.swift
+++ Design12/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text ("SF Symbol weather list")
+                .font(.largeTitle)
+                .padding()
+                
+            ExtractedView(color: .blue, text: "sun.min.fill")
+            ExtractedView(color: .gray, text: "sun.max.fill")
+            ExtractedView(color: .green, text: "sunrise.fill")
+            ExtractedView(color: .orange, text:"sunset.fill")
+            ExtractedView(color:.red, text: "sun.dust.fill")
+            ExtractedView(color:.purple, text: "sun.haze.fill")
+            ExtractedView(color:.red, text: "cloud.fill")
+            ExtractedView(color:.yellow, text: "cloud.drizzle.fill")
+
+   
         }
-        .padding()
     }
 }
 
@@ -23,4 +32,22 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct ExtractedView: View {
+    
+    var color: Color
+    var text: String
+    
+    var body: some View {
+        HStack{
+            Image(systemName:text)
+                .foregroundColor(color)
+            Text(text)
+                .font(.title2)
+                .foregroundColor(color)
+        }
+        .foregroundColor(.red)
+    }
+    
 }
