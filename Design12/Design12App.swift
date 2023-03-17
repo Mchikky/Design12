b94d93a4eaaf14f5d2781fbe3570ade6057f539b
Chikaodili Deng <mchikky@yahoo.com>
2023-03-17 09:37:59 +0100

    Initial Commit

---
+++ Design12/Design12App.swift
@@ -0,0 +1,17 @@
+//
+//  Design12App.swift
+//  Design12
+//
+//  Created by Chikaodili Deng on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Design12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
